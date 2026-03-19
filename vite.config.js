ea8de3376e88e9a783f96c8a5f58e98227a90b3d
Horbin <2240045751@qq.com>
2026-03-19 16:28:36 +0800

    feat: deploy to github page

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
+  base: '/CheckYourWord/',
   plugins: [
     vue(),
     AutoImport({
